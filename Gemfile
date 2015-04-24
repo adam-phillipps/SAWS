8b28d726b5654b61b0b7d51284ab3461384779d8
Adam Phillipps <adam@instructure.com>
2015-04-24 11:37:53 -0600

    add instance existance checking and stub in start_spot

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'mysql2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'minitest', '~> 5.6.0'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -4,14 +4,12 @@ class Contract < ActiveRecord::Base
   before_destroy :stop_instances
   
   def set_name
-    unless self.name
-      self.update(name: self.smash_client.name)
-    end
+    self.update(name: self.smash_client.name) unless self.name
   end
 
   def status( options={} )
-    ec2 = self.smash_client.aws_client
-    ec2.describe_instances( instance_ids: [options[:id]] )[:reservations].first.instances.first[:state].name
+    self.smash_client.aws_client.describe_instances( instance_ids: [options[:id]] )[:reservations].
+      first.instances.first[:state].name
   end # end status
 
   def determine_instance_type_and_start
@@ -20,30 +18,31 @@ class Contract < ActiveRecord::Base
       else
       start_on_demand_instance
     end
-  end
+  end # end determine_instance_type_and_start
 
   def start_on_demand_instance
     ec2 = self.smash_client.aws_client
-    start_instance_with_id( (self.instance_id || get_instance_id) )
+    start_instance_with_id( (self.instance_id || instance_id) )
   end # end_start_on_demand_instance
 
   # TODO find a better way 'cause it has to exist
   # finds the instance id using the name the user gives
-  def get_instance_id
+  def instance_id
     begin
       @instance_id ||= self.smash_client.aws_client.describe_instances(
         filters: [name: 'tag:Name', values: [self.name]]).
           first.reservations.first.instances.first.instance_id # gets an instance id
-      rescue => e
-        "no such instance: #{e}"
-      end
-  end # end get_instance_id
+    rescue => e
+      "no such instance: #{e}"
+    end
+  end # end instance_id
 
 
   # uses an instance id to start an on_demand instance
   def start_instance_with_id( id )
     begin
-      instance = self.smash_client.aws_client.start_instances( instance_ids: [id] ).starting_instances.first
+      instance = self.smash_client.aws_client.
+        start_instances( instance_ids: [id] ).starting_instances.first
       instance_id = instance.instance_id
       begin
         self.update(instance_id: instance_id)
@@ -62,8 +61,9 @@ class Contract < ActiveRecord::Base
   # as the parameter
   def get_ami( options={} )
     image_name = (self.name || options[:name])+'*'
+    byebug
     @image ||= self.smash_client.aws_client.
-      describe_images( owners: ['self'],filters: [ {name: 'name', values: [image_name]} ] ).images.last # returns an Aws::EC2::Image
+      describe_images( owners: ['self'],filters: [ name: 'tag:Name', values: [image_name] ] ).images.last # returns an Aws::EC2::Image
   end # end get_ami
 
   # asks Aws what the best price is for this type of instance, then returns info about the price and zone that had the best value
@@ -78,19 +78,39 @@ class Contract < ActiveRecord::Base
       product_descriptions: [tags.select{|t| t.key.eql? 'product_descriptions' }.first.value],
       availability_zone: az )#.data[:spot_price_history_set]
     end
+    # find a way to include instance_types 
+    byebug
     best_match = spot_prices.each.map(&:spot_price_history).flatten.
-      map{|sp| {spot_price: sp.spot_price, availability_zone: sp.availability_zone }}.min_by{|p| p[:price]}
+      map{|sp| {spot_price: sp.spot_price, availability_zone: sp.availability_zone, instance_type: sp.instance_type }}.min_by{|p| p[:price]}
     best_match[:spot_price] = ( best_match[:spot_price].to_f + 
-      ( best_match[:spot_price].to_f*0.2 ) ).round(3).to_s # TODO: add a method that does this 20% increase, etc.
+      ( best_match[:spot_price].to_f*0.2 )).round(3).to_s # TODO: add a method that does this 20% increase, etc.
     best_match
-  end # best_choice
+  end # best_choice_for
 
+  def instance_already_exists_in( zone )
+    self.smash_client.aws_client.describe_instances( filters: [
+      {name: 'tag:Name', values: [self.name]},
+      {name: 'tag:availability_zone', values: [zone]}]).reservations.count > 0
+  end # end instance_already_exists_in?
+
+  # use the name to find an existing image or find an ami for the instance
+  # use block device mapping to figure out if it has other instances attached to it
+  # if it does, find their amis
+  # start the instance in whatever region
   def start_spot_instance
-    instance = self.smash_client.aws_client.
-      request_spot_instances( spot_instance_params )
+    byebug
+    ec2 = self.smash_client.aws_client
+    best_choice_params = best_choice_for( get_ami)
+    instance = 'nothing yet'
+    if instance_already_exists_in( best_choice_params[:availability_zone])
+      instance = ec2.request_spot_instance( spot_instance_params)
+    else
+      byebug
+    end
+    byebug
     begin
-      ec2.wait_until( :instance_running, instance_ids[@instance.instance_id] )
-      self.update( instance_id: instance.instance_id )
+      ec2.wait_until( :instance_running, instance_ids[@instance.instance_id])
+      self.update( instance_id: instance.instance_id)
       'insance running'
     rescue => e
       "failed updating instance: #{e}"
@@ -98,18 +118,29 @@ class Contract < ActiveRecord::Base
     instance
   end
 
-  def spot_instance_params
-    price_for_zone = best_choice_for( get_ami )
-    {spot_price: price_for_zone[:spot_price],
+  def spot_instance_params( options={} )
+    byebug
+    best_choice_params = best_choice_for( get_ami( options[:name]))
+    {spot_price: best_choice_params[:spot_price],
       instance_count: 1, 
       launch_specification: {
         image_id: get_ami.image_id,
         instance_type: get_ami.tags.select{ |tag| tag.key.eql? "instance_types" }.first.value,
-        placement: {availability_zone: price_for_zone[:availability_zone]}}}
+        placement: {availability_zone: best_choice_params[:availability_zone]},
+        block_device_mappings: get_block_device_mappings}}
+  end
+
+  def get_block_device_mappings
+    self.smash_client.aws_client.describe_images( image_ids: [get_ami.image_id]).first.images.first.block_device_mappings
+  end
+
+  def get_related_instances
+    ec2 = self.smash_client.aws_client
+    ec2.describe_instances
   end
 
   def stop_instances
-    id = (self.instance_id || get_instance_id)
+    id = (self.instance_id || instance_id)
     byebug
     if id.eql? ("no such instance: undefined method `instances' for nil:NilClass") 
       return 'instance is already stopped or does not exist'
